f6f98bba7c37a643a77730879fe3759d34c76420
MichalOlejnik <56115973+MichalOlejnik@users.noreply.github.com>
2019-10-17 14:46:38 +0200

    Add files via upload

---
+++ Podstawy_Programowania/zajecia_3_cd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace zajecia_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Podaj a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b: ");
+            b = int.Parse(Console.ReadLine());
+
+            while (a != b)
+            {
+
+
+                if (a > b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+
+
+            }
+            Console.WriteLine("Największy wspólny dzielnik a i b: " + a);
+
+
+
+            Console.ReadKey();
+        
+    }
+    }
+}
