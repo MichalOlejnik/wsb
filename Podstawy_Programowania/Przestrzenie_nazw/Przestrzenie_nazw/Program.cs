887b3251aff341fc21cc70e11def8bbc86c44795
MichalOlejnik <56115973+MichalOlejnik@users.noreply.github.com>
2019-11-14 14:47:40 +0100

    Add files via upload

---
+++ Podstawy_Programowania/Przestrzenie_nazw/Przestrzenie_nazw/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Second_name;
+using Outer.Middle.Inner;
+
+namespace Przestrzenie_nazw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.Console.WriteLine();      //zamiast "using System" na początku kodu
+            First_name.name_1 ob1 = new First_name.name_1();
+            name_1 ob2 = new name_1();
+            ob1.Show();
+            ob2.Show();
+
+            Prog ob = new Prog();
+            Console.WriteLine("\n"+ob.Add(3, 5));
+            Console.ReadKey();
+        }
+    }
+}
+
+namespace First_name
+{
+    class name_1
+    {
+        public void Show()
+        {
+            Console.WriteLine("Pierwsza przestrzeń nazw");
+        }
+    }
+}
+
+namespace Second_name
+{
+    class name_1
+    {
+        public void Show()
+        {
+            Console.WriteLine("Druga przestrzeń nazw");
+        }
+    }
+}
+
+namespace Outer
+{
+    namespace Middle
+    {
+        namespace Inner
+        {
+            class Prog
+            {
+                public int x, y;
+                public int Add(int x, int y)
+                {
+                    return x + y;
+                }
+            }
+        }
+    }
+}
---
+++ Podstawy_Programowania/funkcje6/funkcje6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Funkcje6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[2];
+            //ta tablica nie ma przypisanych żadnych wartośc do indeksów
+            tab[0] = 5;
+            tab[1] = 10;
+
+            Console.WriteLine("Wartości tablicy przed wywołaniem funkcji: {0} i {1}", tab[0], tab[1]);
+            Tab1(ref tab);
+            Console.WriteLine("Wartości tablicy po wywołaniu funkcji: {0} i {1}", tab[0], tab[1]);
+
+            ShowTab(tab);
+            Console.ReadKey();
+        }
+
+        static void Tab1(ref int[] tab)
+        {
+            tab[0] = 50;
+            tab[1] = 100;
+            Console.WriteLine("Wartośći tablicy wewnątrz funkcji: {0} i {1}", tab[0], tab[1]);
+        }
+
+        static void ShowTab(int[] tab)
+        {
+            Console.Write("Wartości w tablicy: ");
+            foreach (int item in tab)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+
+    }
+}
