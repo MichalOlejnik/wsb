a927f101bd70531e67445debc3b79c6f035e67ff
MichalOlejnik <56115973+MichalOlejnik@users.noreply.github.com>
2019-11-28 14:21:49 +0100

    Add files via upload

---
+++ Podstawy_Programowania/Wskazniki_4/Wskazniki_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Wskazniki_4
+{
+    class Program
+    {
+
+        struct Test
+        {
+           public int n;
+        }
+
+
+        unsafe static void Main(string[] args)
+        {
+            Test x = new Test();
+            Test* p = &x;
+
+            //przypisywanie wartości do wskaźnika
+            // p->n=20;      lub         (*p).n=20;
+
+            Console.WriteLine(x.n);
+        }
+    }
+}
