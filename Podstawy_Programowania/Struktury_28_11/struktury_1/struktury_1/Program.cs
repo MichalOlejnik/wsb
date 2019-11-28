e974933b6203858fe3aa5f56498dbdeda0ec94b8
MichalOlejnik <56115973+MichalOlejnik@users.noreply.github.com>
2019-11-28 13:17:22 +0100

    Add files via upload

---
+++ Podstawy_Programowania/Struktury_28_11/struktury_1/struktury_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace struktury_1
+{
+    class Program
+    {
+
+        public struct Punkt
+        {
+            public int x;
+            public int y;
+        }
+        static void Main(string[] args)
+        {
+            Punkt punkt1 = new Punkt();
+
+            punkt1.x = 5;
+            punkt1.y = 15;
+
+            Punkt punkt2= new Punkt();
+
+            Console.WriteLine("Punkt 1: x={0}, y={1}", punkt1.x, punkt1.y);
+            Console.WriteLine("Punkt 2: x={0}, y={1}", punkt2.x, punkt2.y);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
