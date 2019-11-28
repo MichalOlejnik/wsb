07550d8ecd914e9af5182dc36802accaa7f5e437
MichalOlejnik <56115973+MichalOlejnik@users.noreply.github.com>
2019-11-28 14:15:24 +0100

    Add files via upload

---
+++ Podstawy_Programowania/Wskazniki_3/Wskazniki_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Wskazniki_3
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int[] tab = { 10, 20, 30, 40, 50 };
+
+            fixed(int* x=tab)                   //fixed do używania wskaźników w tablicach
+                for(int i=0; i<tab.Length; i++)
+                {
+                    Console.WriteLine("Adres tab[{0}] = {1}", i, (int)x+i);
+                    Console.WriteLine(", wartość {0}", *(x+i));
+                }
+            Console.ReadKey();
+        }
+    }
+}
