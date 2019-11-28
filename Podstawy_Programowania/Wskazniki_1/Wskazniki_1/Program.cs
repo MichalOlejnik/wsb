a08ec7131f41895e6ba04200e1d1f86da6b1a433
MichalOlejnik <56115973+MichalOlejnik@users.noreply.github.com>
2019-11-28 13:27:06 +0100

    Add files via upload

---
+++ Podstawy_Programowania/Wskazniki_1/Wskazniki_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Wskazniki_1
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)  //przy wskaźnikach dodajemy unsafe!
+        {
+            char* x;            //w Projekt-->właściwości--> kompilacja--> zezwalaj na niebezpieczny kod
+            char letter = 'x';
+            x = &letter;        // x ma teraz przypisany adres zmiennej letter, & - operator adresowania
+            letter = 'a';
+            //2 sposoby wyświetlania
+            //*- operator zwracający zmienną przechowywaną
+            Console.WriteLine("znak: {0}", letter);
+            Console.WriteLine("znak: {0}", *x);     //pamiętaj o * przed x, * - kopiuje wartość danej zmiennej
+            //Console.WriteLine("Adres w pamięci zmiennej letter: {0:x} ", (int)x); //zamiana na HEX
+
+            int test = 10;
+            Console.WriteLine(Convert.ToString(test, 8));   //zamiana na ósemkowy
+
+            double number;
+            double* numberAddress;
+            numberAddress = &number;
+
+            number = 10;
+            Console.WriteLine("Liczba wynosi: {0}", number);
+            Console.WriteLine("Liczba wynosi: {0}", numberAddress->ToString());       //Operator wskaźnika do składowej "->"
+            Console.WriteLine("Adres w pamięci zmiennej letter: {0} ", (int)numberAddress);
+
+            Console.ReadKey();
+        }
+    }
+}
